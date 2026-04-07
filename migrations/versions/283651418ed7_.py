ccf37aeed63b67a853651373fd4570a9290ebf91
Patrick <patcthomas28@gmail.com>
2026-04-07 00:09:47 +0000

    Completed merge with Jin's code

--- migrations/versions/283651418ed7_.py
+++ migrations/versions/283651418ed7_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0763d677d453
+Revision ID: 283651418ed7
 Revises: 
-Create Date: 2025-02-25 14:47:16.337069
+Create Date: 2026-04-07 00:06:45.628952
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0763d677d453'
+revision = '283651418ed7'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,8 +23,11 @@ def upgrade():
     sa.Column('email', sa.String(length=120), nullable=False),
     sa.Column('password', sa.String(), nullable=False),
     sa.Column('is_active', sa.Boolean(), nullable=False),
+    sa.Column('user_name', sa.String(length=80), nullable=True),
+    sa.Column('date_of_birth', sa.String(length=32), nullable=True),
     sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('email')
+    sa.UniqueConstraint('email'),
+    sa.UniqueConstraint('user_name')
     )
     # ### end Alembic commands ###
 
